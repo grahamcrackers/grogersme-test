03b4d6ac3d0ec27aafa502eddc9b8fabc58f0d52
grahamcrackers <gmrogers04@gmail.com>
2015-04-16 20:16:33 -0500

    adding images

---
+++ scripts/grogers.js
@@ -0,0 +1,11 @@
+'use strict';
+
+$(document).ready(function(){
+
+    // background for intro
+    $(".intro").backstretch('images/mac.jpg');
+
+    // background for about
+    //$(".about").backstretch('images/crashing_waves.jpeg');
+
+});
